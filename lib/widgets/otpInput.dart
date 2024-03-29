baa99f64802ed47b73310e17f78b0068fbebe9f1
ali <ali@alis-MacBook-Air.local>
2024-03-29 16:05:23 +0330

    otp page fix

--- lib/widgets/otpInput.dart
+++ lib/widgets/otpInput.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:pinput/pinput.dart';
 
 class OtpInput extends StatefulWidget {
