4575878c82c53d09aefa75da68695b932616200d
ali <ali@alis-MacBook-Air.local>
2024-03-30 14:43:43 +0330

    home page added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:shoonz_tailor/widgets/homeWidgets/customerChart.dart';
 import 'package:shoonz_tailor/widgets/homeWidgets/topProfile.dart';
 
 class Home extends StatefulWidget {
@@ -18,7 +19,55 @@ class _HomeState extends State<Home> {
         top: Platform.isAndroid ? false : true,
         child: Column(
           children: [
-            TopProfile(phoneNumber: "09122758432", tailorName: "نام خیاط")
+            const TopProfile(
+                phoneNumber: "09122758432", tailorName: "نام خیاط"),
+            const SizedBox(
+              height: 20,
+            ),
+            const CustomerChart(),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: Colors.grey[200],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: () {},
+                      child: const Text(
+                        "مشاهده همه",
+                        textDirection: TextDirection.rtl,
+                        style: TextStyle(
+                          color: Color(0xff005200),
+                        ),
+                      )),
+                  const Row(
+                    children: [
+                      Text(
+                        "مشتریان",
+                        textAlign: TextAlign.end,
+                        textDirection: TextDirection.rtl,
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Icon(
+                        Icons.supervisor_account_outlined,
+                        size: 35,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
---
+++ lib/widgets/homeWidgets/customerChart.dart
@@ -0,0 +1,98 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomerChart extends StatefulWidget {
+  const CustomerChart({super.key});
+
+  @override
+  State<CustomerChart> createState() => _CustomerChartState();
+}
+
+class _CustomerChartState extends State<CustomerChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height / 3.7,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: const Color(0xffb2cab2),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "تعداد سفارشات : ۱۰۰ عدد",
+                textAlign: TextAlign.end,
+                textDirection: TextDirection.rtl,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    decoration: TextDecoration.underline),
+              ),
+              Text(
+                "فروش",
+                textAlign: TextAlign.end,
+                textDirection: TextDirection.rtl,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: Container(
+              width: MediaQuery.sizeOf(context).width,
+              child: LineChart(
+                LineChartData(
+                  baselineY: 100,
+                  maxX: 10,
+                  minX: 0,
+                  minY: 30,
+                  maxY: 100,
+                  borderData: FlBorderData(
+                      border: const Border.fromBorderSide(BorderSide.none)),
+                  gridData: const FlGridData(
+                      drawHorizontalLine: false, drawVerticalLine: true),
+                  titlesData: const FlTitlesData(
+                    topTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: false,
+                      ),
+                    ),
+                    rightTitles: AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                  ),
+                  lineBarsData: [
+                    LineChartBarData(
+                        isCurved: true,
+                        barWidth: 2,
+                        color: const Color(0xff005200),
+                        spots: [
+                          const FlSpot(0, 25),
+                          const FlSpot(2, 40),
+                          const FlSpot(3.5, 80),
+                          const FlSpot(5, 40),
+                          const FlSpot(6, 50),
+                          const FlSpot(8, 90),
+                          const FlSpot(10, 90),
+                        ]),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
